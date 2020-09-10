bdf2d12588ed44a922e990743e709a7a6750ebe2
Glizida <vladislav.gajdukevich@gmail.com>
2020-09-11 02:13:49 +0300

    Тесты работы с TreeView

--- TreeNotes/TreeNotes.xaml.cs
+++ TreeNotes/TreeNotes.xaml.cs
@@ -1,28 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using TreeNotes.Class;
+using Newtonsoft.Json.Linq;
 
 namespace TreeNotes
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
+
     public partial class MainWindow : Window
     {
+        struct datanotes
+        {
+
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+
+
         }
     }
 }
