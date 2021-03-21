35c9b933c0d7d8716012b0a7c5fef95d73407a50
Vladislav Gaidukevich <vladislav.gajdukevich@gmail.com>
2021-03-21 22:23:13 +0300

    Базовый функционал с примером, от него и будем отталкиватся

--- TreeNotes/TreeNotes.xaml.cs
+++ TreeNotes/TreeNotes.xaml.cs
@@ -3,13 +3,14 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Windows;
 using System.Xml.Serialization;
+using Newtonsoft.Json;
 
 namespace TreeNotes
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    
+
 
     public partial class MainWindow : Window
     {
@@ -18,50 +19,87 @@ namespace TreeNotes
         {
             public string Name { get; set; }
             public ObservableCollection<Node> Nodes { get; set; }
+            public string Text { get; set; }
 
             public Node(string name, ObservableCollection<Node> nodes)
             {
                 Name = name;
                 Nodes = nodes;
+                Text = "";
             }
+
+            public Node(string name, ObservableCollection<Node> nodes, string text)
+            {
+                Name = name;
+                Nodes = nodes;
+                Text = text;
+            }
+
 
             public Node()
             {
+                Text = "";
             }
         }
 
         ObservableCollection<Node> nodes;
+        string Path = Environment.CurrentDirectory + "\\tree.json";
+
+        Node selectNode;
 
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();           
+        }
 
-            nodes = new ObservableCollection<Node>
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            //-------------------------Сохранение дерева и текста в файл--------------------------//
+
+            string jsonOut = JsonConvert.SerializeObject(nodes);
+            using (StreamWriter sw = new StreamWriter(Path, false, System.Text.Encoding.Default))
             {
-                new Node()
-                {
-                    Name = "Нода 1",
-                    Nodes = new ObservableCollection<Node>
-                    {
-                        new Node()
-                        {
-                            Name = "Нода 2"
-                        }
-                    }
-                },
-                new Node()
-                {
-                    Name = "Нода 3"
-                }
-            };
-            
-            XmlSerializer formatter = new XmlSerializer(typeof(Node));
-            using (FileStream fs = new FileStream("wons.xml", FileMode.Create))
-            {
-                formatter.Serialize(fs, nodes);
+                sw.WriteLine(jsonOut);
             }
+        }
 
-            TreeViewNotes.ItemsSource = nodes;
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //--------------------Загрузка дерева и текста из файла---------------------------//
+            if (File.Exists(Path)) 
+            {
+                nodes = new ObservableCollection<Node> { };
+                string jsonText = "";
+                using (StreamReader sr = new StreamReader(Path))
+                {
+                    jsonText = sr.ReadToEnd();
+                }
+                nodes = JsonConvert.DeserializeObject<ObservableCollection<Node>>(jsonText);
+                TreeView.ItemsSource = nodes;
+            }
+            else
+            {
+                File.Create(Path);
+            }
+               
+
+        }
+
+        private void TreeView_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            //--------------------Загрузка текта привязанного к ноде---------------------------//
+            selectNode = (Node)TreeView.SelectedItem;
+            TextBox.Text = selectNode.Text;
+        }
+
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            //--------------------Изменение текта привязанного к ноде и его сохранения---------------------------//
+            int index = nodes.IndexOf(selectNode);
+            selectNode.Text = TextBox.Text;
+            nodes[index] = selectNode;
+            TreeView.ItemsSource = nodes;
+            
         }
     }
 }
